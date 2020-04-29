1063e2982e56c9ac9d4fd1ef1d0cfb68f667ddcb
joelsannerstedt <joel.sannerstedt@gmail.com>
2020-04-29 12:11:09 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data/test.json';
+import Line from './components/charts/Line';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '500px' }}>
+      <Line data={data} legendBottom="Bottom" legendLeft="left" />
     </div>
   );
 }
